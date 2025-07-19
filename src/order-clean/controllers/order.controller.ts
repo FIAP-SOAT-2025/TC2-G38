5e9b4eaf0b062264bced5a6eda5a80bf9ba44d1b
Luiz Resplande <resplandeluiz@Mac-mini-de-Luiz.local>
2025-07-19 10:43:54 -0300

    improvement: instanciandos o gateway para chamar o useCase

--- src/order-clean/controllers/order.controller.ts
+++ src/order-clean/controllers/order.controller.ts
@@ -1,26 +1,39 @@
+import { OrderGateway } from '../gateways/order.gateway';
 import { OrderDto } from '../infraestructure/api/dto/order.dto';
 import { UpdateOrderStatusDto } from '../infraestructure/api/dto/update-status.dto';
 import OrderGatewayInterface from '../interfaces/gateways';
 
 export class OrderController {
-  constructor() {}
+  constructor() { }
 
   static async createOrder(
     createOrderDto: OrderDto,
     orderRepository: OrderGatewayInterface,
   ) {
-    // return await this.orderService.process(createOrderDto);
+    const orderGateway = new OrderGateway(orderRepository);
+    try {
+
+    } catch (error) {
+      throw new Error('Failed to fetch customer by CPF');
+    }
   }
 
   static async find(id: string, orderRepository: OrderGatewayInterface) {
+    const orderGateway = new OrderGateway(orderRepository);
     // return await this.listOrderService.find(id);
   }
 
   static async findAll(orderRepository: OrderGatewayInterface) {
+    const orderGateway = new OrderGateway(orderRepository);
     // return await this.listOrderService.findAll();
   }
 
-  async updateStatus(id: string, statusDto: UpdateOrderStatusDto) {
+  async updateStatus(
+    id: string,
+    statusDto: UpdateOrderStatusDto,
+    orderRepository: OrderGatewayInterface,
+  ) {
+    const orderGateway = new OrderGateway(orderRepository);
     // return await this.updateOrderService.updateStatus(id, statusDto.status);
   }
 }
