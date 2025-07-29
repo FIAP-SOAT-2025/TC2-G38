227c8470076f065fc3b6c7690b9901664b0ba6d0
Daniela-Rego <danielalimaqueiroz@gmail.com>
2025-07-28 22:58:53 -0300

    padronizando erros do fluxo do item,incluindo validaçao item existe

--- src/item/useCases/findItem.useCase.ts
+++ src/item/useCases/findItem.useCase.ts
@@ -6,9 +6,9 @@ import { BaseException } from "src/shared/exceptions/exceptions.base";
 
 export default class FindItemUseCase {
  static async findById(id: string,  itemGateway: ItemGatewayInterface): Promise<Item | null> {
-        const notDeleteItem = await itemGateway.findByIdIfNotDeleted(id, false);
-        return new Item(notDeleteItem); 
-     
+        const itemExists = await itemGateway.findByIdIfNotDeleted(id, false);
+        return new Item(itemExists); 
+
     }
 
 } 
